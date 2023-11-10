1ee4e209a17f40c1723296a00be571cd0e0c2d0c
Amarnath2813 <72410948+Amarnath2813@users.noreply.github.com>
2023-11-10 23:42:39 +0530

    Amar

---
+++ Day7WA/WA_Cookieexample/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WA_Cookieexample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Create()
+        {
+            string key = "DemoCookie";
+            string value = "Amar";
+
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(7);
+            Response.Cookies.Append(key, value, options);
+            return View();
+        }
+        public IActionResult Read() {
+            string key = "DemoCookie";
+            var CookieValue = Request.Cookies[key];
+            ViewBag.CookieValue = CookieValue;
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            string key = "";
+            string value = DateTime.Now.ToString();
+
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Append(key, value, options);
+
+            return View();
+        }
+    }
+}
---
+++ Day7WA/WA_Cookieexample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WA_Cookieexample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
